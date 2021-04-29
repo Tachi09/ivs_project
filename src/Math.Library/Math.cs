5e2f2a2ac30e299a8a3c4b130e7c52508689ec7d
Tachi09 <79200206+Tachi09@users.noreply.github.com>
2021-04-29 20:25:12 +0200

    Update Math.cs

--- src/Math.Library/Math.cs
+++ src/Math.Library/Math.cs
@@ -1,21 +1,57 @@
-﻿using System;
+/****************************************************
+ * Název projektu: Kalkulačka
+ * Soubor: Math.cs
+ * Datum: 20.4.2021
+ * Poslední změna: 28.4.2021
+ * Autor: ASSC tým
+ * 
+ * Popis: Matematická knihovna
+ * 
+ ****************************************************
+ /**
+ * @file Math.cs
+ * 
+ * @brief Třída Math - knihovna s matematickými operacemi
+ * @autor ASSC tým
+ */
+using System;
 
 namespace Math.Library
 {
-    public class Math
+    public class Math : IMath
     {
-        public static double Abs(double n1)
+        /**
+         * @brief Absolutní hodnota
+         * 
+         * @param n1 Číslo, které chceme převézt na absolutní hodnotu
+         * @return Číslo v absolutní hodnotě
+         */
+        public double Abs(double n1)
         {
             if (n1 < 0) return -n1;
             return n1; 
         }
 
-        public static double Add(double n1, double n2) //sčítání
+        /**
+         * @brief Matematický součet (sčítání)
+         * 
+         * @param n1 První sčítanec
+         * @param n2 Druhý sčítanec
+         * @return Součet sčítanců
+         */
+        public double Add(double n1, double n2)
         {
             return n1 + n2;
         }
 
-        public static double Div(double n1, double n2) //dělení
+        /**
+         * @brief Matematický podíl (dělení)
+         * 
+         * @param n1 Dělenec
+         * @param n2 Dělitel
+         * @return Podíl čísel
+         */
+        public double Div(double n1, double n2)
         {
             if (n2 == 0)
             {
@@ -23,8 +59,13 @@ namespace Math.Library
             }
             return n1 / n2;
         }
-
-        public static long Fact(int n) //faktoriál
+        /**
+         * @brief Faktoriál
+         * 
+         * @param n Kladné celé číslo
+         * @return Faktoriál čísla
+         */
+        public long Fact(int n)
         {
             int factorial = 1;
             for(int i = 1; i <= n; i++)
@@ -33,19 +74,38 @@ namespace Math.Library
             }
             return factorial;
         }
-
-        public static double Mul(double n1, double n2) //násobení
+        /**
+         * @brief Matematické součin (násobení)
+         * 
+         * @param n1 První činitel
+         * @param n2 Druhý činitel
+         * @return Součin činitelů
+         */
+        public double Mul(double n1, double n2)
         {
             return n1 * n2;
         }
 
-
-        public static double Pow(double n1, double n2) //N1 umocněno na N2
+        /**
+         * @brief N-tá mocnina čísla
+         * 
+         * @param n1 Základ mocniny
+         * @param n2 Mocnina
+         * @return Umocněné číslo
+         */
+        public double Pow(double n1, double n2)
         {
             return System.Math.Pow(n1, n2);
         }
 
-        public static double Sqrt(double n1, double n2) //odmocnina
+        /**
+         * @brief N-ta odmocnina čísla
+         * 
+         * @param n1 Základ odmocniny
+         * @param n2 Exponent mocnina
+         * @return Odmocněné číslo
+         */
+        public double Sqrt(double n1, double n2)
         {
             if (n1 <= 0) return -1;
             else
@@ -55,8 +115,14 @@ namespace Math.Library
                 return root;
             }
         }
-
-        public static double Sub(double n1, double n2)//odčítá N2 od N1 a vrací N1
+        /**
+         * @brief Matematický rozdíl (odečítání)
+         * 
+         * @param n1 Menšenec
+         * @param n2 Menšitel
+         * @return Rozdíl čísel
+         */
+        public double Sub(double n1, double n2)
         {
             return n1 - n2;
         }
